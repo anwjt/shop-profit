f08cb0c29501adeb163dae19699dea009602be43
Momo Jaroh <momojaroh@gmail.com>
2025-07-25 13:04:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -4,7 +4,7 @@ import serviceAccount from '../../serviceAccountKey.json';
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
     });
   } catch (error) {
     console.error('Firebase admin initialization error', error);
